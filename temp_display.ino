8b7a32e87a0907dcf916efce8dd7a1c50945907b
anatoliis <15053145+anatoliis@users.noreply.github.com>
2025-03-10 03:50:21 +0100

    best version

--- temp_display.ino
+++ temp_display.ino
@@ -4,204 +4,213 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>
 
-#define READING_WINDOW_MS 1000
+#define TFT_ALMOSTBLACK 0x0861 /*  10,  10,  10 */
+#define TFT_ERRORGRAY 0x3183   /*  50,  25,  25 */
+
+#define READING_TIMEOUT_MS 50
 #define GREY_OUT_DELAY_MS 5000
-#define TURN_OFF_DELAY_MS 15000
-#define SERIAL_BUFFER_SIZE 10
+
+#define CMD_RESET 0x1E
+#define CMD_REPEAT 0x07
+#define DATA_MODE 0xD0
+#define ACK 0x06
+#define NAK 0x15
+
+uint8_t temps[3] = { 0, 0, 0 };
+uint8_t prevDisplayedTemps[3] = { 0, 0, 0 };
+
+uint8_t tempsBuffer[3];
+
+bool areValuesGreyedOut = true;
+bool isBacklightEnabled = false;
+
+unsigned long lastUpdate = millis();
+
+typedef struct {
+  bool isInitialized;
+  float processNoiseCovariance;
+  float measurementNoiseCovariance;
+  float errorCovariance;
+  float kalmanGain;
+  float currentEstimate;
+  unsigned long lastUpdate;
+} KalmanFilter;
+
+KalmanFilter kalmanFilters[3] = {};
 
 TFT_eSPI tft = TFT_eSPI();
 
-typedef struct {
-  bool initialized;
-  float Q;
-  float R;
-  float P;
-  float K;
-  float current_estimate;
-} KalmanFilter;
-
-KalmanFilter cpu_temp_kf;
-KalmanFilter gpu_temp_kf;
-KalmanFilter nvme_temp_kf;
-
-void prepare_kf(KalmanFilter* kf) {
-  kf->initialized = false;
-  kf->current_estimate = 0.0;
-  kf->P = 1.0;
-  kf->Q = 0.5;
-  kf->R = 0.5;
-}
-
-uint8_t update_kf(KalmanFilter* kf, uint8_t current_temp) {
-  if (!kf->initialized) {
-    kf->current_estimate = current_temp;
-    kf->initialized = true;
-  } else if (current_temp > kf->current_estimate) {
-    kf->current_estimate = current_temp;
-    kf->P += kf->Q;
-  } else {
-    kf->P = kf->P + kf->Q;
-    kf->K = kf->P / (kf->P + kf->R);
-    kf->current_estimate = kf->current_estimate + kf->K * ((float)current_temp - kf->current_estimate);
-    kf->P = (1 - kf->K) * kf->P;
-  }
-  return round(kf->current_estimate);
-}
-
 void setup(void) {
   tft.init();
-  toggle_display(0);
-  tft.setRotation(2);
+  toggleDisplayForce(0);
+  tft.setRotation(2);  // portrait upside down
   tft.setTextSize(1);
   tft.fillScreen(TFT_BLACK);
 
-  prepare_kf(&cpu_temp_kf);
-  prepare_kf(&gpu_temp_kf);
-  prepare_kf(&nvme_temp_kf);
+  resetTemperatures();
 
-  Serial.begin(115200);
+  Serial.setTimeout(READING_TIMEOUT_MS);
+  Serial.begin(9600);
 }
 
-uint8_t prev_cpu_temp = 0;
-uint8_t prev_gpu_temp = 0;
-uint8_t prev_nvme_temp = 0;
-unsigned long last_update = millis();
-bool grey_out = true;
+void resetTemperatures() {
+  resetKalmanFilters();
+  for (uint8_t i = 0; i < 3; i++) {
+    temps[i] = 0;
+    prevDisplayedTemps[i] = 0;
+  }
+}
 
-void update(char* buffer) {
-  char cpu_temp_char[4];
-  char gpu_temp_char[4];
-  char nvme_temp_char[4];
+void prepareKalmanFilter(KalmanFilter* kf) {
+  kf->isInitialized = false;
+  kf->currentEstimate = 0.0;
+  kf->errorCovariance = 1.0;
+  kf->processNoiseCovariance = 0.5;
+  kf->measurementNoiseCovariance = 0.5;
+}
 
-  strncpy(cpu_temp_char, buffer, 3);
-  strncpy(gpu_temp_char, buffer + 3, 3);
-  strncpy(nvme_temp_char, buffer + 6, 3);
-
-  uint8_t cpu_temp = update_kf(&cpu_temp_kf, atoi(cpu_temp_char));
-  uint8_t gpu_temp = update_kf(&gpu_temp_kf, atoi(gpu_temp_char));
-  uint8_t nvme_temp = update_kf(&nvme_temp_kf, atoi(nvme_temp_char));
-
-  grey_out = false;
-
-  if (temperature_did_change(cpu_temp, gpu_temp, nvme_temp)) {
-    draw_values(cpu_temp, gpu_temp, nvme_temp);
-    prev_cpu_temp = cpu_temp;
-    prev_gpu_temp = gpu_temp;
-    prev_nvme_temp = nvme_temp;
+uint8_t updateKalmanFilter(KalmanFilter* kf, uint8_t currentTemp) {
+  if (currentTemp == 0) {
+    if (kf->isInitialized) {
+      prepareKalmanFilter(kf);
+    }
+    return 0;
+  }
+  if (!kf->isInitialized) {
+    kf->currentEstimate = currentTemp;
+    kf->isInitialized = true;
+    kf->lastUpdate = millis();
+    return currentTemp;
   }
 
-  toggle_display(1);
-  last_update = millis();
+  unsigned long currentTime = millis();
+  unsigned long timeDiff = currentTime - kf->lastUpdate;
+  float adjustedProcessNoiseCovariance = kf->processNoiseCovariance * (1 + timeDiff / 1000.0);
+
+  kf->errorCovariance = kf->errorCovariance + adjustedProcessNoiseCovariance;
+  kf->kalmanGain = kf->errorCovariance / (kf->errorCovariance + kf->measurementNoiseCovariance);
+  kf->currentEstimate = kf->currentEstimate + kf->kalmanGain * ((float)currentTemp - kf->currentEstimate);
+  kf->errorCovariance = (1 - kf->kalmanGain) * kf->errorCovariance;
+  kf->lastUpdate = currentTime;
+  return round(kf->currentEstimate);
 }
 
-inline bool temperature_did_change(uint8_t cpu_temp, uint8_t gpu_temp, uint8_t nvme_temp) {
-  return cpu_temp != prev_cpu_temp || gpu_temp != prev_gpu_temp || nvme_temp != prev_nvme_temp;
+void resetKalmanFilters() {
+  for (uint8_t i = 0; i < 3; i++) {
+    prepareKalmanFilter(&kalmanFilters[i]);
+  }
 }
-
-char buffer[SERIAL_BUFFER_SIZE] = {};
-unsigned long packet_receipt_start_time = 0;
 
 void loop() {
-  int8_t receive_index = -1;
-
-  while (1) {
-    if (Serial.available()) {
-      char incomingByte = Serial.read();
-      if (receive_index == -1) {
-        if (is_data_packet_start_byte(incomingByte)) {
-          packet_receipt_start_time = millis();
-          receive_index = 0;
-          continue;
-        } else if (is_previous_packet_repeat_byte(incomingByte)) {
-          receive_index = 1;
-          break;
-        } else if (is_stop_byte(incomingByte)) {
-          send_ack();
-          stop();
-          return;
-        }
-        break;
-      }
-      if (is_data_packet_end_byte(incomingByte) || receive_index >= SERIAL_BUFFER_SIZE - 1) {
-        buffer[receive_index] = '\0';
-        break;
-      }
-      // received next byte from data packet
-      buffer[receive_index] = incomingByte;
-      receive_index++;
+  while (!Serial.available()) {
+    if (!areValuesGreyedOut && (millis() - lastUpdate) >= GREY_OUT_DELAY_MS) {
+      areValuesGreyedOut = true;
+      drawValues(prevDisplayedTemps);
     }
-
-    if (reading_window_expired()) {
-      // serial read timeout, starting over
-      receive_index = -1;
+  }
+  switch (Serial.read()) {
+    case DATA_MODE:
+      if (receiveTemperatures(temps)) {
+        sendAck();
+        update(temps);
+      } else {
+        sendNak();
+      }
       break;
-    }
+    case CMD_REPEAT:
+      sendAck();
+      update(temps);
+      break;
+    case CMD_RESET:
+      sendAck();
+      reset();
+      break;
+    default:
+      sendNak();
   }
+}
 
-  if (receive_index > 0) {
-    send_ack();
-    update(buffer);
-  } else if (grey_out_delay_elapsed()) {
-    grey_out = true;
-  } else if (turn_off_delay_elapsed()) {
-    stop();
+bool receiveTemperatures(uint8_t* targetTemps) {
+  if (Serial.readBytes(tempsBuffer, 3) != 3) {
+    return false;
   }
+  memcpy(targetTemps, tempsBuffer, 3);
+  return true;
 }
 
-inline bool is_data_packet_start_byte(char byte) {
-  return byte == 0x02;
+inline void sendAck() {
+  writeByte(ACK);
 }
 
-inline bool is_previous_packet_repeat_byte(char byte) {
-  return byte == 0x01;
+inline void sendNak() {
+  writeByte(NAK);
 }
 
-inline bool is_stop_byte(char byte) {
-  return byte == 0x04;
+inline void writeByte(char byte) {
+  discardSerialInput();
+  Serial.write(byte);
+  Serial.flush();
 }
 
-inline bool is_data_packet_end_byte(char byte) {
-  return byte == 0x03;
+inline void discardSerialInput() {
+  while (Serial.available()) Serial.read();
 }
 
-inline bool reading_window_expired() {
-  return (millis() - packet_receipt_start_time) > READING_WINDOW_MS;
+void update(uint8_t* inputTemps) {
+  uint8_t displayedTemps[3];
+  for (uint8_t i = 0; i < 3; i++) {
+    displayedTemps[i] = updateKalmanFilter(&kalmanFilters[i], inputTemps[i]);
+  }
+  memcpy(prevDisplayedTemps, displayedTemps, sizeof(displayedTemps));
+  areValuesGreyedOut = false;
+  drawValues(displayedTemps);
+  lastUpdate = millis();
 }
 
-inline bool grey_out_delay_elapsed() {
-  return (millis() - last_update) >= GREY_OUT_DELAY_MS;
+void drawValues(uint8_t* inputTemps) {
+  // cpu
+  drawSingleValue(inputTemps[0], 89, 0);
+  // nvme
+  drawSingleValue(inputTemps[1], 70, 1);
+  // gpu
+  drawSingleValue(inputTemps[2], 82, 2);
+
+  toggleDisplay(1);
 }
 
-inline bool turn_off_delay_elapsed() {
-  return (millis() - last_update) >= TURN_OFF_DELAY_MS;
+inline void drawSingleValue(uint8_t value, uint8_t threshold, uint8_t lineIndex) {
+  uint16_t verticalPosition = 2 + 81 * lineIndex;
+  if (value == 0) {
+    tft.setTextColor(TFT_ALMOSTBLACK, TFT_BLACK);
+    tft.drawString("00", 14, verticalPosition, 8);
+    return;
+  }
+  char str[3];
+  uint16_t color = (lineIndex % 2 == 0) ? TFT_LIGHTGREY : TFT_DARKGREY;
+  if (areValuesGreyedOut) {
+    color = TFT_ERRORGRAY;
+  } else if (value >= threshold) {
+    color = TFT_RED;
+  }
+  tft.setTextColor(color, TFT_BLACK);
+  tft.drawString(itoa(value, str, 10), 14, verticalPosition, 8);
 }
 
-inline void send_ack() {
-  Serial.write(0x06);
-}
-
-void stop() {
-  toggle_display(0);
+void reset() {
+  Serial.flush();
+  discardSerialInput();
+  toggleDisplayForce(0);
   tft.fillScreen(TFT_BLACK);
+  resetTemperatures();
 }
 
-void draw_values(uint8_t cpu_temp, uint8_t gpu_temp, uint8_t nvme_temp) {
-  char buffer[SERIAL_BUFFER_SIZE];
-  tft.setTextColor(grey_out ? TFT_DARKGREY : (cpu_temp >= 89 ? TFT_RED : TFT_LIGHTGREY), TFT_BLACK);
-  tft.drawString(itoa(cpu_temp, buffer, 10), 14, 2, 8);
-
-  tft.setTextColor(grey_out ? TFT_DARKGREY : (nvme_temp >= 70 ? TFT_GOLD : TFT_DARKGREY), TFT_BLACK);
-  tft.drawString(itoa(nvme_temp, buffer, 10), 14, 83, 8);
-  
-  tft.setTextColor(grey_out ? TFT_DARKGREY : (gpu_temp >= 82 ? TFT_RED : TFT_LIGHTGREY), TFT_BLACK);
-  tft.drawString(itoa(gpu_temp, buffer, 10), 14, 164, 8);
-}
-
-bool is_backlight_enabled = 1;
-
-void toggle_display(uint8_t value) {
-  if (value != is_backlight_enabled) {
-    digitalWrite(TFT_BL, value);
-    is_backlight_enabled = value;
+inline void toggleDisplay(bool value) {
+  if (value != isBacklightEnabled) {
+    toggleDisplayForce(value);
   }
+}
+
+inline void toggleDisplayForce(bool value) {
+  digitalWrite(TFT_BL, value);
+  isBacklightEnabled = value;
 }
